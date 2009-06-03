4853fcf54d5ddb2c7066600db2cad30251dfe55d
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-06-03 17:37:09 +0400

    zip_safe version.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dzenlog-link',
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'This is a simple application to post links to the dzenlog.',
     keywords = 'django apps blogging dzenlog',
     license = 'New BSD License',
     author = 'Alexander Artemenko',
     author_email = 'svetlyak.40wt@gmail.com',
     url = 'http://github.com/svetlyak40wt/django-dzenlog-link/',
-    install_requires = ['django-dzenlog>=0.2.0'],
+    install_requires = ['django-dzenlog>=0.2.7'],
     dependency_links = ['http://pypi.aartemenko.com/'],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -25,5 +25,6 @@ setup(
         'templates': ['*.html'],
     },
     include_package_data = True,
+    zip_safe = False,
 )
 
